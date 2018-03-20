06ec3c1c5b3c801b4c5fef08486eaf9cb662e7ab
Vladimir Bazhenov <vbazhenov@yandex-team.ru>
2018-03-20 13:37:33 +0300

    Use SearchManager in search example

--- src/main/java/com/yandex/mapkitdemo/SearchActivity.java
+++ src/main/java/com/yandex/mapkitdemo/SearchActivity.java
@@ -8,14 +8,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.yandex.mapkit.GeoObjectCollection;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraListener;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.CameraUpdateSource;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.map.MapObjectCollection;
+import com.yandex.mapkit.map.VisibleRegionUtils;
 import com.yandex.mapkit.mapview.MapView;
+import com.yandex.mapkit.search.Response;
+import com.yandex.mapkit.search.SearchManagerType;
 import com.yandex.mapkit.search.SearchOptions;
-import com.yandex.mapkit.search_layer.SearchLayer;
-import com.yandex.mapkit.search_layer.SearchResultListener;
+import com.yandex.mapkit.search.SearchManager;
+import com.yandex.mapkit.search.Session;
 import com.yandex.runtime.Error;
+import com.yandex.runtime.image.ImageProvider;
 import com.yandex.runtime.network.NetworkError;
 import com.yandex.runtime.network.RemoteError;
 
@@ -24,7 +33,7 @@ import com.yandex.runtime.network.RemoteError;
  * Note: search API calls count towards MapKit daily usage limits. Learn more at
  * https://tech.yandex.ru/mapkit/doc/3.x/concepts/conditions-docpage/#conditions__limits
  */
-public class SearchActivity extends Activity implements SearchResultListener {
+public class SearchActivity extends Activity implements Session.SearchListener, CameraListener {
     /**
      * Replace "your_api_key" with a valid developer key.
      * You can get it at the https://developer.tech.yandex.ru/ website.
@@ -32,10 +41,16 @@ public class SearchActivity extends Activity implements SearchResultListener {
     private final String MAPKIT_API_KEY = "your_api_key";
 
     private MapView mapView;
-    private SearchLayer searchLayer;
+    private EditText searchEdit;
+    private SearchManager searchManager;
+    private Session searchSession;
 
     private void submitQuery(String query) {
-        searchLayer.submitQuery(query, new SearchOptions());
+        searchSession = searchManager.submit(
+                query,
+                VisibleRegionUtils.toPolygon(mapView.getMap().getVisibleRegion()),
+                new SearchOptions(),
+                this);
     }
 
     @Override
@@ -45,25 +60,26 @@ public class SearchActivity extends Activity implements SearchResultListener {
         setContentView(R.layout.search);
         super.onCreate(savedInstanceState);
 
-        final EditText searchEdit = (EditText)findViewById(R.id.search_edit);
+        searchManager = MapKitFactory.getInstance().createSearchManager(SearchManagerType.COMBINED);
+
+        mapView = (MapView)findViewById(R.id.mapview);
+        mapView.getMap().addCameraListener(this);
+
+        searchEdit = (EditText)findViewById(R.id.search_edit);
         searchEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     submitQuery(searchEdit.getText().toString());
-                    return true;
                 }
 
                 return false;
             }
         });
 
-        mapView = (MapView)findViewById(R.id.mapview);
         mapView.getMap().move(
                 new CameraPosition(new Point(59.945933, 30.320045), 14.0f, 0.0f, 0.0f));
 
-        searchLayer = mapView.getMap().getSearchLayer();
-        searchLayer.addSearchResultListener(this);
         submitQuery(searchEdit.getText().toString());
     }
 
@@ -82,11 +98,18 @@ public class SearchActivity extends Activity implements SearchResultListener {
     }
 
     @Override
-    public void onSearchStart() {
-    }
+    public void onSearchResponse(Response response) {
+        MapObjectCollection mapObjects = mapView.getMap().getMapObjects();
+        mapObjects.clear();
 
-    @Override
-    public void onSearchSuccess() {
+        for (GeoObjectCollection.Item searchResult : response.getCollection().getChildren()) {
+            Point resultLocation = searchResult.getObj().getGeometry().get(0).getPoint();
+            if (resultLocation != null) {
+                mapObjects.addPlacemark(
+                        resultLocation,
+                        ImageProvider.fromResource(this, R.drawable.search_result));
+            }
+        }
     }
 
     @Override
@@ -99,5 +122,16 @@ public class SearchActivity extends Activity implements SearchResultListener {
         }
 
         Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onCameraPositionChanged(
+            Map map,
+            CameraPosition cameraPosition,
+            CameraUpdateSource cameraUpdateSource,
+            boolean finished) {
+        if (finished) {
+            submitQuery(searchEdit.getText().toString());
+        }
     }
 }
