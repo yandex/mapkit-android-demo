56aade6849e1f1b6d361adbcdd7af457aa33a050
asybil <skarlat-denis@mail.ru>
2020-04-25 18:13:49 +0300

    Fix security exception for User Location (checked and tested on API 23). Changed AndroidManifest and UserLocationActivity

--- src/main/java/com/yandex/mapkitdemo/UserLocationActivity.java
+++ src/main/java/com/yandex/mapkitdemo/UserLocationActivity.java
@@ -1,9 +1,13 @@
 package com.yandex.mapkitdemo;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.PointF;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
@@ -12,6 +16,7 @@ import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.CompositeIcon;
 import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.Map;
 import com.yandex.mapkit.map.MapWindow;
 import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
@@ -32,22 +37,34 @@ public class UserLocationActivity extends Activity implements UserLocationObject
 
     private MapView mapView;
     private UserLocationLayer userLocationLayer;
+    private final int MY_PERMISSION_REQUEST = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         MapKitFactory.setApiKey(MAPKIT_API_KEY);
         MapKitFactory.initialize(this);
         setContentView(R.layout.user_location);
         super.onCreate(savedInstanceState);
+        /**
+         * Check permission that may to get user location.
+         */
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            /**
+             * Request permission. You can use ACCESS_COARSE_LOCATION or ACCESS_FINE_LOCATION
+             */
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSION_REQUEST);
+        }
         mapView = findViewById(R.id.mapview);
         mapView.getMap().setRotateGesturesEnabled(false);
         mapView.getMap().move(new CameraPosition(new Point(0, 0), 14, 0, 0));
-
         MapKit mapKit = MapKitFactory.getInstance();
         userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
         userLocationLayer.setVisible(true);
         userLocationLayer.setHeadingEnabled(true);
-
         userLocationLayer.setObjectListener(this);
     }
 
