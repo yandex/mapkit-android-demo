1059553206c8da12a6b1ad96a63062cdfaa74b41
Egor Basharin <egorbasharin@yandex-team.ru>
2020-08-08 04:42:15 +0300

    Add selection example

---
+++ src/main/java/com/yandex/mapkitdemo/SelectionActivity.java
@@ -0,0 +1,99 @@
+package com.yandex.mapkitdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.layers.GeoObjectTapEvent;
+import com.yandex.mapkit.layers.GeoObjectTapListener;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.GeoObjectSelectionMetadata;
+import com.yandex.mapkit.map.InputListener;
+import com.yandex.mapkit.map.Map;
+import com.yandex.mapkit.mapview.MapView;
+
+/**
+ * This example show how to activate selection.
+ */
+public class SelectionActivity extends Activity implements GeoObjectTapListener, InputListener {
+    /**
+     * Replace "your_api_key" with a valid developer key.
+     * You can get it at the https://developer.tech.yandex.ru/ website.
+     */
+    private final String MAPKIT_API_KEY = "0dfe3cfc-261d-4a74-a746-e7cdc258e66c";
+    private final Point TARGET_LOCATION = new Point(59.936760, 30.314673);
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        /**
+        * Set the api key before calling initialize on MapKitFactory.
+        * It is recommended to set api key in the Application.onCreate method,
+        * but here we do it in each activity to make examples isolated.
+        */
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        /**
+        * Initialize the library to load required native libraries.
+        * It is recommended to initialize the MapKit library in the Activity.onCreate method
+        * Initializing in the Application.onCreate method may lead to extra calls and increased battery use.
+        */
+        MapKitFactory.initialize(this);
+        // Now MapView can be created.
+        setContentView(R.layout.map);
+        super.onCreate(savedInstanceState);
+        mapView = (MapView)findViewById(R.id.mapview);
+
+        // And to show what can be done with it, we move the camera to the center of Saint Petersburg.
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 17.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 1),
+                null);
+
+        mapView.getMap().addTapListener(this);
+        mapView.getMap().addInputListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        // Activity onStop call must be passed to both MapView and MapKit instance.
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // Activity onStart call must be passed to both MapView and MapKit instance.
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public boolean onObjectTap(@NonNull GeoObjectTapEvent geoObjectTapEvent) {
+        final GeoObjectSelectionMetadata selectionMetadata = geoObjectTapEvent
+                .getGeoObject()
+                .getMetadataContainer()
+                .getItem(GeoObjectSelectionMetadata.class);
+
+        if (selectionMetadata != null) {
+            mapView.getMap().selectGeoObject(selectionMetadata.getId(), selectionMetadata.getLayerId());
+        }
+
+        return selectionMetadata != null;
+    }
+
+    @Override
+    public void onMapTap(@NonNull Map map, @NonNull Point point) {
+        mapView.getMap().deselectGeoObject();
+    }
+
+    @Override
+    public void onMapLongTap(@NonNull Map map, @NonNull Point point) {
+
+    }
+}
